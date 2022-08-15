33493fccc1d6afc89ac6ce3f6485c16a4588de4c
willy <willy@hallo.app>
2022-08-15 16:01:37 -0300

    creating Container Component style

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,10 @@
+import { styled } from "centaurus-styles";
+
+export const HeaderStyles = styled("header", {
+  height: "100px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  background: "$magenta",
+  color: "$light-solid",
+});
